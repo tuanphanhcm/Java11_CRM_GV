9cabfc428ec909c9aecb0079c10fbbfcc7dddc22
ngavothibich <56902528+ngavothibich@users.noreply.github.com>
2021-05-05 20:32:58 +0700

    Update AuthServlet.java
    test

--- src/cybersoft/java11/crm/servlet/AuthServlet.java
+++ src/cybersoft/java11/crm/servlet/AuthServlet.java
@@ -17,7 +17,7 @@ public class AuthServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
 	}
 
 }
