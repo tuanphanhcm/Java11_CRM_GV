cb3f9956cce6e828e8e8ee92fe02130679d28f23
ngavothibich <56902528+ngavothibich@users.noreply.github.com>
2021-05-05 20:25:00 +0700

    update code
    test

---
+++ src/cybersoft/java11/crm/servlet/AuthServlet.java
@@ -0,0 +1,12 @@
+package cybersoft.java11.crm.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "authServlet", urlPatterns = {
+		"/login",
+		"/logout"
+		"/register"
+})
+public class AuthServlet extends HttpServlet{
+
+}
