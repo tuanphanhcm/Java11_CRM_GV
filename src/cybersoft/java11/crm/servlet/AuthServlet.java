468d0c34e9197b97c8987f3d4c70884c52c9bdef
ngavothibich <56902528+ngavothibich@users.noreply.github.com>
2021-05-05 20:27:18 +0700

    Update AuthServlet.java

--- src/cybersoft/java11/crm/servlet/AuthServlet.java
+++ src/cybersoft/java11/crm/servlet/AuthServlet.java
@@ -1,12 +1,23 @@
 package cybersoft.java11.crm.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "authServlet", urlPatterns = {
 		"/login",
-		"/logout"
+		"/logout",
 		"/register"
 })
 public class AuthServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
 
 }
