acbcc68d25bbec71a28bf33bdaec651daac3b5d9
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2021-05-12 19:53:36 +0700

    update add role function

--- src/cybersoft/java11/crm/dao/RoleDao.java
+++ src/cybersoft/java11/crm/dao/RoleDao.java
@@ -1,6 +1,7 @@
 package cybersoft.java11.crm.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -35,8 +36,44 @@ public class RoleDao {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
 		}
 		
 		return listRole;
+	}
+	
+	public int add(Role role) {
+		int result = -1;
+		Connection connection = MySqlConnection.getConnection();
+		
+		try {
+			String query = "insert role(`name`, `description`) values(?, ?)";
+			
+			PreparedStatement statement = connection.prepareStatement(query);
+			
+			statement.setString(1, role.getName());
+			statement.setString(2, role.getDescription());
+			
+			result = statement.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+		}
+		
+		return result;
 	}
 }
--- src/cybersoft/java11/crm/servlet/RoleServlet.java
+++ src/cybersoft/java11/crm/servlet/RoleServlet.java
@@ -45,7 +45,7 @@ public class RoleServlet extends HttpServlet {
 				req.getRequestDispatcher(JspPathConst.ROLE_DASHBOARD).forward(req, resp);
 				break;
 			case UrlConst.ROLE_ADD:
-				
+				req.getRequestDispatcher(JspPathConst.ROLE_ADD).forward(req, resp);
 				break;
 			case UrlConst.ROLE_UPDATE:
 				
@@ -58,6 +58,44 @@ public class RoleServlet extends HttpServlet {
 			break;
 		}
 		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String path = req.getServletPath();
 		
+		switch (path) {
+			case UrlConst.ROLE_DASHBOARD:
+				
+				break;
+			case UrlConst.ROLE_ADD:
+				String roleName = req.getParameter("role-name");
+				String roleDescription = req.getParameter("role-description");
+				
+				if(roleName == null || roleName.equals("")) {
+					req.setAttribute("msg", "Role name can't be empty.");
+					req.getRequestDispatcher(JspPathConst.ROLE_ADD).forward(req, resp);
+				} else {
+					Role newRole = new Role();
+					newRole.setName(roleName);
+					newRole.setDescription(roleDescription);
+					
+					biz.add(newRole);
+					
+					resp.sendRedirect(req.getContextPath() + UrlConst.ROLE_DASHBOARD);
+				}
+				
+				break;
+			case UrlConst.ROLE_UPDATE:
+				
+				break;
+			case UrlConst.ROLE_DELETE:
+				
+				break;
+
+		default:
+			break;
+		}
 	}
 }
